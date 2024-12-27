865077b28a60472f79027df0472580aae60cf552
tikami <hahoanganh3210@gmail.com>
2024-12-27 16:50:32 +0700

    update 27/12/2024 v1

--- frontend/src/redux/apiRequest.js
+++ frontend/src/redux/apiRequest.js
@@ -1,14 +1,34 @@
 import axios from "axios";
-import { loginFailed, loginStart, loginSuccess} from "./authSlice";
+import {
+  loginFailed,
+  loginStart,
+  loginSuccess,
+  registerFailed,
+  registerStart,
+  registerSuccess,
+} from "./authSlice";
 
-export const loginUser = async(user,dispatch,navigate)=>{
-    dispatch(loginStart());
-    try {
-        console.log(`${process.env.REACT_APP_API_URL}/v1/auth/login`)
-        const res = await axios.post(`${process.env.REACT_APP_API_URL}/v1/auth/login`,user);
-        dispatch(loginSuccess(res.data));
-        navigate("/");
-    }catch(err){
-        dispatch(loginFailed());
-    }
+export const loginUser = async (user, dispatch, navigate) => {
+  dispatch(loginStart());
+  try {
+    const res = await axios.post(
+      `${process.env.REACT_APP_API_URL}/v1/auth/login`,
+      user
+    );
+    dispatch(loginSuccess(res.data));
+    navigate("/");
+  } catch (err) {
+    dispatch(loginFailed());
+  }
+};
+
+export const registerUser = async (user, dispatch, navigate) => {
+  dispatch(registerStart());
+  try {
+    await axios.post(`${process.env.REACT_APP_API_URL}/v1/auth/register`, user);
+    dispatch(registerSuccess());
+    navigate("/login");
+  } catch (err) {
+    dispatch(registerFailed());
+  }
 };
--- frontend/src/redux/authSlice.js
+++ frontend/src/redux/authSlice.js
@@ -1,34 +1,55 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
-    name : "auth",
-    initialState : {
-        login:{
-            currentUser: null,
-            isFetching: false,
-            error: false
-        }
+  name: "auth",
+  initialState: {
+    login: {
+      currentUser: null,
+      isFetching: false,
+      error: false,
     },
-    reducers:{
-        loginStart: (state)=>{
-            state.login.isFetching = true;
-        },
-        loginSuccess: (state,action) => {
-            state.login.isFetching = false;
-            state.login.currentUser = action.payload;
-            state.login.error = false;
-        },
-        loginFailed: (state) =>{
-            state.login.isFetching = false;
-            state.login.edrror = true;
-        }
-    }
+    register: {
+      isFetching: false,
+      error: false,
+      success: false,
+    },
+  },
+  reducers: {
+    loginStart: (state) => {
+      state.login.isFetching = true;
+    },
+    loginSuccess: (state, action) => {
+      state.login.isFetching = false;
+      state.login.currentUser = action.payload;
+      state.login.error = false;
+    },
+    loginFailed: (state) => {
+      state.login.isFetching = false;
+      state.login.edrror = true;
+    },
+    registerStart: (state) => {
+      state.register.isFetching = true;
+    },
+    registerSuccess: (state, action) => {
+      state.register.isFetching = false;
+      state.register.error = false;
+      state.register.success = true;
+    },
+    registerFailed: (state) => {
+      state.register.isFetching = false;
+      state.register.error = true;
+      state.register.success = false;
+    },
+  },
 });
 
 export const {
-    loginStart,
-    loginFailed,
-    loginSuccess
+  loginStart,
+  loginFailed,
+  loginSuccess,
+  registerStart,
+  registerSuccess,
+  registerFailed,
 } = authSlice.actions;
 
 export default authSlice.reducer;
